54a3874428fe66e6e567192718f347ba49d1d8f5
Jaime <jaimeloeuf@gmail.com>
2020-05-14 18:37:36 +0800

    Initial Commit:     Website build from vuetify's personal theme.

---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-btn
+      v-for="(item, i) in items"
+      :key="i"
+      fab
+      class="ma-2"
+      color="info"
+      :small="true"
+    >
+      <v-icon dark v-text="item.icon" />
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    large: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: () => ({
+    items: [
+      {
+        icon: "mdi-github-circle",
+      },
+      {
+        icon: "mdi-linkedin-box",
+      },
+      {
+        icon: "mdi-email",
+      },
+      {
+        icon: "mdi-phone",
+      },
+    ],
+  }),
+};
+</script>
---
+++ src/components/core/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer height="88" class="justify-center">
+    <social-media />
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "CoreFooter",
+
+  components: {
+    SocialMedia: () => import("@/components/SocialMedia"),
+  },
+};
+</script>
